00d5d00cbe71d6a47aa1934cd790715a0cc96a29
lukaskass <lukaskas@wix.com>
2026-04-28 13:33:51 +0300

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className="text-3xl font-bold text-red-500">Hello</h1>
+      <div className="flex items-center justify-center h-screen">
+        <Button>shadcn works! 🎉</Button>
+      </div>
     </>
   )
 }
